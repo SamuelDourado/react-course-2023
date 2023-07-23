8aaf20dfe3ce808709d07b169d08da0eb28bfb8d
Samuel Dourado <sd7038@gmail.com>
2023-07-23 15:34:01 -0300

    Adiciona lógica para adicionar novos item do Expenses

--- expense-tracker/src/components/NewExpense/ExpenseForm.js
+++ expense-tracker/src/components/NewExpense/ExpenseForm.js
@@ -1,19 +1,66 @@
 import "./ExpenseForm.css";
+import {useState} from 'react';
 
 const ExpenseForm = () => {
-    return <form>
+
+    const [enteredTitle, setEnteredTitle] = useState('');
+    const [enteredAmount, setEnteredAmount] = useState('');
+    const [enteredDate, setEnteredDate] = useState('');
+
+    const titleChangeHandler = (event) => {
+        console.log(event.target.value);
+        setEnteredTitle(event.target.value);
+    };
+
+    const amountChangeHandler = (event) => {
+        console.log(event.target.value);
+        setEnteredAmount(event.target.value);
+    };
+
+    const dateChangeHandler = (event) => {
+        console.log(event.target.value);
+        setEnteredDate(event.target.value);
+    };
+
+    /**
+     * 77. Alternative: Creating A Shared Handler Function
+     * onChange={(event) => inputChangeHandler(indentifier, event.target.value)}
+     */
+    // const inputChangeHandler = {identifier, value} => {
+    //     if(identifier == 'title') {
+    //         setEnteredTitle(value);
+    //     } else if(identifier == 'date') {
+    //         setEnteredDate(value);
+    //     } else {
+    //         setEnteredAmount(value);
+    //     }
+    // }
+
+    const submitHandler = (event) => {
+        event.preventDefault();
+
+        const expenseData = {
+            title: enteredTitle,
+            date: new Date(enteredDate),
+            amount: enteredAmount
+        };
+
+        console.log(expenseData);
+    };
+
+    return <form onSubmit={submitHandler}>
         <div className="new-expense__controls">
             <div className="new-expense__control">
                 <label>Title</label>
-                <input type="text" />
+                <input type="text" onChange={titleChangeHandler} />
             </div>
             <div className="new-expense__control">
                 <label>Amount</label>
-                <input type="number" min="0.01" step="0.01" />
+                <input type="number" onChange={amountChangeHandler} min="0.01" step="0.01" />
             </div>
             <div className="new-expense__control">
                 <label>Date</label>
-                <input type="date" min="2019-01-01" max="2022-12-31"/>
+                <input type="date" onChange={dateChangeHandler} min="2019-01-01" max="2022-12-31"/>
             </div>
         </div>
         <div class="new-expense__actions">
