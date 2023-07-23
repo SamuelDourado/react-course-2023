2389438b5ab6f264e5b54c8b7fe1ba3a187b062a
Samuel Dourado <sd7038@gmail.com>
2023-07-23 15:52:10 -0300

    Adiciona two-way binding para limpar formulário ao submiter o formulário de NewExpense

--- expense-tracker/src/components/NewExpense/ExpenseForm.js
+++ expense-tracker/src/components/NewExpense/ExpenseForm.js
@@ -46,21 +46,24 @@ const ExpenseForm = () => {
         };
 
         console.log(expenseData);
+        setEnteredAmount('');
+        setEnteredDate('');
+        setEnteredTitle('');
     };
 
     return <form onSubmit={submitHandler}>
         <div className="new-expense__controls">
             <div className="new-expense__control">
                 <label>Title</label>
-                <input type="text" onChange={titleChangeHandler} />
+                <input type="text" onChange={titleChangeHandler} value={enteredTitle} />
             </div>
             <div className="new-expense__control">
                 <label>Amount</label>
-                <input type="number" onChange={amountChangeHandler} min="0.01" step="0.01" />
+                <input type="number" onChange={amountChangeHandler} min="0.01" step="0.01" value={enteredAmount} />
             </div>
             <div className="new-expense__control">
                 <label>Date</label>
-                <input type="date" onChange={dateChangeHandler} min="2019-01-01" max="2022-12-31"/>
+                <input type="date" onChange={dateChangeHandler} min="2019-01-01" max="2022-12-31" value={enteredDate}/>
             </div>
         </div>
         <div class="new-expense__actions">
