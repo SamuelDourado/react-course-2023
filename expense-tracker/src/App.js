0665855d46c20a00276ac0ac6320ef845008700b
Samuel Dourado <sd7038@gmail.com>
2023-07-24 13:24:08 -0300

    Adiciona lógica para atualizar lista de Expenses

--- expense-tracker/src/App.js
+++ expense-tracker/src/App.js
@@ -1,31 +1,35 @@
+import { useState } from "react";
 import ExpenseList from "./components/Expenses/ExpenseList";
 import NewExpense from "./components/NewExpense/NewExpense";
 
+const DUMMY_EXPENSES = [
+  {
+    id: "e1",
+    title: "Toilet Paper",
+    amount: 12.5,
+    date: new Date(2023, 7, 17),
+  },
+  {
+    id: "e2",
+    title: "TV",
+    amount: 700,
+    date: new Date(2023, 7, 10),
+  },
+  {
+    id: "e3",
+    title: "New Desk",
+    amount: 400,
+    date: new Date(2023, 7, 20),
+  },
+];
+
 function App() {
-  const expenses = [
-    {
-      id: "e1",
-      title: "Toilet Paper",
-      amount: 12.5,
-      date: new Date(2023, 7, 17),
-    },
-    {
-      id: "e2",
-      title: "TV",
-      amount: 700,
-      date: new Date(2023, 7, 10),
-    },
-    {
-      id: "e3",
-      title: "New Desk",
-      amount: 400,
-      date: new Date(2023, 7, 20),
-    },
-  ];
+ const[expenses, setExpenses] = useState(DUMMY_EXPENSES);
 
   const addExpenseHandler = expense => {
-    console.log("In App.js");
-    console.log(expense);
+    setExpenses(prevState => {
+      return [expense, ...prevState];
+    });
   };
 
   return (
