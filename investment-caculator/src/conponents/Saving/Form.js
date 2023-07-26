9b432f68a77754d44e812ff99a4eaa796836ebbc
Samuel Dourado <sd7038@gmail.com>
2023-07-26 08:04:08 -0300

    Task2: manipulando submit e reset event

--- investment-caculator/src/conponents/Saving/Form.js
+++ investment-caculator/src/conponents/Saving/Form.js
@@ -1,8 +1,39 @@
+import {useState} from 'react';
 import style from './Form.module.css';
 
 function Form() {
+  const [currentSavings, setCurrentSavings] = useState(0);
+  const [yearlySavings, setYearlySavings] = useState(0);
+  const [expectedInterest, setExpectedInterest] = useState(0);
+  const [investmentDuration, setInvestmentDuration] = useState(0);
+
+  const changeCurrentSavingsHandler = (event) => {
+    setCurrentSavings(event.target.value);
+  };
+
+  const changeYearlySavingsHandler = event => {
+    setYearlySavings(event.target.value);
+  };
+
+  const changeExpectedInterestHandler = event => {
+    setExpectedInterest(event.target.value);
+  };
+
+  const changeInvestmentDurationHandler = event => {
+    setInvestmentDuration(event.target.value);
+  };
+
+  const onSubmitHandler = (event) => {
+    event.preventDefault();
+    console.log("Form submited!");
+  };
+
+  const onReset = () => {
+    console.log("Form Reseted!");
+  };
+
   return (
-    <form className={style.form}>
+    <form onSubmit={onSubmitHandler} className={style.form}>
       <div className={style['input-group']}>
         <p>
           <label htmlFor="current-savings">Current Savings ($)</label>
@@ -26,7 +57,7 @@ function Form() {
         </p>
       </div>
       <p className={style.actions}>
-        <button type="reset" className={style.buttonAlt}>
+        <button type="reset" onClick={onReset} className={style.buttonAlt}>
           Reset
         </button>
         <button type="submit" className={style.button}>
