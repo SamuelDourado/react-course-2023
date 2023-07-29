37abdc1ee7a5c28cc6fcf0506ccc48bd84397e0f
Samuel Dourado <sd7038@gmail.com>
2023-07-29 14:32:14 -0300

    Adiciona ErrorModal conponent

--- add-user/src/components/UI/ErrorModal.js
+++ add-user/src/components/UI/ErrorModal.js
@@ -1,5 +1,24 @@
-function ErrorModal() {
+import Card from "./Card";
+import Button from "./Button";
+import style from "./ErrorModal.module.css";
 
+function ErrorModal(props) {
+  return (
+    <div>
+      <div className={style.backdrop}></div>
+      <Card className={style.modal}>
+        <header className={style.header}>
+          <h2>{props.title}</h2>
+        </header>
+        <div className={style.content}>
+          <p>{props.message}</p>
+        </div>
+        <footer className={style.actions}>
+          <Button>Okay</Button>
+        </footer>
+      </Card>
+    </div>
+  );
 }
 
 export default ErrorModal;
--- add-user/src/components/User/AddUser.js
+++ add-user/src/components/User/AddUser.js
@@ -1,26 +1,27 @@
-import {useState} from 'react';
+import { useState } from "react";
 import Card from "../UI/Card";
 import Button from "../UI/Button";
 import style from "./AddUser.module.css";
+import ErrorModal from "../UI/ErrorModal";
 
 const INITIAL_USER = {
-  username: '',
-  age: '0'
+  username: "",
+  age: "0",
 };
 
 function AddUser(props) {
   const [userInput, setUserInput] = useState(INITIAL_USER);
 
-  const validateUser = user => {
-    if(user.username.trim().lenhth === 0) {
+  const validateUser = (user) => {
+    if (user.username.trim().lenhth === 0) {
       return false;
     }
 
-    if(user.age.trim().lenhth === 0) {
+    if (user.age.trim().lenhth === 0) {
       return false;
     }
 
-    if(+user.age < 1) {
+    if (+user.age < 1) {
       return false;
     }
     return true;
@@ -29,33 +30,52 @@ function AddUser(props) {
   const addUserHandler = (event) => {
     event.preventDefault();
 
-    if(!validateUser(userInput)) {
+    if (!validateUser(userInput)) {
       return;
     }
 
-    props.onAddUser({id: Math.random(), ...userInput});
+    props.onAddUser({ id: Math.random(), ...userInput });
     setUserInput(INITIAL_USER);
   };
 
   const changeInput = (name, value) => {
-    setUserInput(prevState => {
+    setUserInput((prevState) => {
       return {
         ...prevState,
-        [name]: value
+        [name]: value,
       };
     });
   };
 
   return (
-    <Card className={style.input}>
-      <form onSubmit={addUserHandler}>
+    <div>
+      <ErrorModal title="An error occured!" message="Something went wrong!" />
+      <Card className={style.input}>
+        <form onSubmit={addUserHandler}>
           <label htmlFor="username">Username</label>
-          <input id="username" type="text" name="username" value={userInput.username} onChange={event => {changeInput('username', event.target.value)}} />
+          <input
+            id="username"
+            type="text"
+            name="username"
+            value={userInput.username}
+            onChange={(event) => {
+              changeInput("username", event.target.value);
+            }}
+          />
           <label htmlFor="age">Age(Years)</label>
-          <input id="age" type="number" name="age" value={userInput.age} onChange={event => {changeInput('age', event.target.value)}} />
+          <input
+            id="age"
+            type="number"
+            name="age"
+            value={userInput.age}
+            onChange={(event) => {
+              changeInput("age", event.target.value);
+            }}
+          />
           <Button type="submit"> Add User </Button>
-      </form>
-    </Card>
+        </form>
+      </Card>
+    </div>
   );
 }
 
