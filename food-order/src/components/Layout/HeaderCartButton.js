3e7a047051359e8f75cf5f38210a7023a05820a6
Samuel Dourado <sd7038@gmail.com>
2023-08-05 20:28:51 -0300

    Adiciona lógica para fechar a modal do Cart Componente

--- food-order/src/components/Layout/HeaderCartButton.js
+++ food-order/src/components/Layout/HeaderCartButton.js
@@ -1,8 +1,8 @@
 import CartIcon from '../Cart/CartIcon';
 import classes from './HeaderCartButton.module.css';
 
-function HeaderCartButton() {
-    return <button className={classes.button}>
+function HeaderCartButton(props) {
+    return <button className={classes.button} onClick={props.onClick}>
         <span className={classes.icon}>
             <CartIcon />
         </span>
